d6df7636bfd3d40c94ff9e37d7fdb4d81be1d49e
pu4ron <40077152+pu4ron@users.noreply.github.com>
2020-06-11 13:07:26 -0300

    Add Brazilian Portuguese file.

---
+++ firmware/source/user_interface/languages/portugues_brazil.h
@@ -0,0 +1,166 @@
+/* -*- coding: windows-1252-unix; -*- */
+/* 
+ * Copyright (C)2019 Roger Clark. VK3KYY / G4KYF
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+/*
+ * Translators: PU4RON
+ *
+ *
+ * Rev: 2
+ */
+#ifndef USER_INTERFACE_LANGUAGES_PORTUGUESE_H_
+#define USER_INTERFACE_LANGUAGES_PORTUGUESE_H_
+/********************************************************************
+ *
+ * MUITO IMPORTANTE.
+ * Este arquivo não deve ser salvo com a codificação UTF-8
+ * Use o Notepad ++ no Windows com codificação ANSI
+ * ou emacs no Linux com codificação windows-1252-unix
+ *
+ ********************************************************************/
+const stringsTable_t portuguesLanguage =
+{
+.LANGUAGE_NAME 			= "Portugues BR",
+.language				= "Idioma",
+.menu					= "Menu",
+.credits				= "Créditos",
+.zone					= "Zona",
+.rssi					= "RSSI",
+.battery				= "Bateria",
+.contacts				= "Contatos",
+.firmware_info			= "Firmware",
+.last_heard				= "Último escutado",
+.options				= "Opções",
+.display_options		= "Opções display",
+.sound_options			= "Opções de áudio", 
+.channel_details		= "Detalhes Canal",
+.new_contact			= "Novo contato",
+.new_channel			= "Novo canal", 
+.contact_list			= "Lista contatos",
+.hotspot_mode			= "Modo Hotspot",
+.contact_details		= "Detalhe contato",
+.built					= "Compilado",
+.zones					= "Zonas",
+.keypad					= "Teclado",
+.ptt 					= "PTT",
+.locked 				= "Bloqueado",
+.press_blue_plus_star	= "Aperte azul + *",
+.to_unlock				= "Desbloquear",
+.unlocked				= "Desbloqueado",
+.power_off				= "Desligando...",
+.error					= "ERRO",
+.rx_only				= "Apenas Rx",
+.out_of_band			= "FORA DA BANDA",
+.timeout				= "TEMPO ESGOTADO",
+.tg_entry				= "Inserir TG",
+.pc_entry				= "Inserir CP",
+.user_dmr_id			= "DMRID usuário",
+.contact 				= "Contato",
+.accept_call			= "Aceitar chamada?",
+.private_call			= "Chamada privada",
+.squelch				= "Squelch",
+.quick_menu 			= "Menu rápido",
+.filter					= "Filtro",
+.all_channels			= "Todos canais",
+.gotoChannel			= "Ir para",
+.scan					= "Busca",
+.channelToVfo			= "Canal -> VFO",
+.vfoToChannel			= "VFO -> Canal",
+.vfoToNewChannel		= "VFO -> NovoCanal",
+.group					= "Grupo",
+.private				= "Privado",
+.all					= "Todos",
+.type					= "Tipo",
+.timeSlot				= "TimeSlot", 
+.none					= "Nenhum",
+.contact_saved			= "Contato gravado",
+.duplicate				= "Duplicado",
+.tg						= "TG",
+.pc						= "CP",
+.ts						= "TS",
+.mode					= "Modo",
+.colour_code			= "Códig cor",
+.n_a					= "N/A",
+.bandwidth				= "Larg banda",
+.stepFreq				= "Step",
+.tot					= "TOT",
+.off					= "Off",
+.zone_skip				= "Ignorar Z",
+.all_skip				= "Ignorar T",
+.yes					= "Sim",
+.no						= "Não",
+.rx_group				= "Rx Grp",
+.on						= "On",
+.timeout_beep			= "Bip timeout",
+.factory_reset			= "RESET",
+.calibration			= "Ajustes",
+.band_limits			= "Limite banda",
+.beep_volume			= "Volume bip",
+.dmr_mic_gain			= "Micro DMR",
+.fm_mic_gain			= "Micro FM", 
+.key_long				= "Key long",
+.key_repeat				= "Key rpt",
+.dmr_filter_timeout		= "Filtro DMR",
+.brightness				= "Brilho",
+.brightness_off			= "Min brilho",
+.contrast				= "Contraste",
+.colour_invert			= "Cor:Invertido",
+.colour_normal			= "Cor:Normal",
+.backlight_timeout		= "Timeout",
+.scan_delay				= "Scan delay",
+.YES					= "SIM",
+.NO						= "NÃO",
+.DISMISS				= "DISPENSAR",
+.scan_mode				= "Modo Scan",
+.hold					= "Hold",
+.pause					= "Pausa",
+.empty_list				= "Lista vazia",
+.delete_contact_qm		= "Apagar contato?",
+.contact_deleted		= "Contato apagado",
+.contact_used			= "Contato usado",
+.in_rx_group			= "no grupo Rx",
+.select_tx				= "Selecione TX",
+.edit_contact			= "Editar contato",
+.delete_contact			= "Apagar contato",
+.group_call				= "Chamada grupo",
+.all_call				= "Todas chamadas",
+.tone_scan				= "Tone scan",
+.cc_scan				= "CC scan",
+.low_battery			= "BATERIA FRACA!",
+.Auto					= "Auto", 
+.manual					= "Manual", 
+.ptt_toggle				= "PTT toggle", 
+.private_call_handling	= "Handle CP", 
+.stop					= "Stop", 
+.one_line				= "1 linha",
+.two_lines				= "2 linhas",
+.priority_order			= "Ordem",
+.dmr_beep				= "DMR bip",
+.start					= "Start",
+.both					= "Both",
+.vox_threshold          = "VOX Thres.",
+.vox_tail               = "VOX Tail"
+};
+/*********************************************************************
+ *
+ * MUITO IMPORTANTE.
+ * Este arquivo não deve ser salvo com a codificação UTF-8
+ * Use o Notepad ++ no Windows com codificação ANSI
+ * ou emacs no Linux com codificação windows-1252-unix
+ *
+ ********************************************************************/
+#endif /* USER_INTERFACE_LANGUAGES_PORTUGUESE_H_ */
